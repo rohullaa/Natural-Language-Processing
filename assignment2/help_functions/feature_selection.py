528a6dd5f9e0c299dc18b708dba8b8b291b72f9a
Rohullah Akbari <54434823+rohullaa@users.noreply.github.com>
2021-10-23 00:24:53 +0200

    edited

--- assignment2/help_functions/feature_selection.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-%matplotlib inline
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso, LogisticRegression
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import StandardScaler
